7b8f0aa610986c3e4ed8f8e2ec16c58de5f6768f
JulSeb42 <julien.sebag@me.com>
2024-11-30 19:21:45 +0100

    Fix plop

--- plop/generators/page.ts
+++ plop/generators/page.ts
@@ -6,15 +6,33 @@ export default (plop: NodePlopAPI) => {
     const { setGenerator } = plop
 
     setGenerator("page", {
-        description: "",
+        description: "Creates a page",
         prompts: [
             { type: "input", message: "Enter page's name", name: "name" },
+            {
+                type: "input",
+                message: "Enter path",
+                name: "path",
+                default: data => `/${data.name.toLowerCase()}`,
+            },
         ],
         actions: [
             {
                 type: "add",
                 templateFile: "./templates/page.hbs",
                 path: "../src/views/{{>pascalName}}View.vue",
+            },
+            {
+                type: "modify",
+                path: "../src/router/index.ts",
+                template: `import {{>pascalName}}View from "@/views/{{>pascalName}}View.vue"\n$1`,
+                pattern: /(\/\* Prepend import - DO NOT REMOVE \*\/)/g,
+            },
+            {
+                type: "modify",
+                path: "../src/router/index.ts",
+                template: `{\n\t\t\tpath: "{{ path }}",\n\t\t\tname: "{{>pascalName}}",\n\t\t\tcomponent: {{>pascalName}}View,\n\t\t\tmeta: { title: SITE_DATA.PAGE_TITLE("{{>pascalName}}") },\n\t\t},\n\t\t$1`,
+                pattern: /(\/\* Prepend route - DO NOT REMOVE \*\/)/g,
             },
         ],
     })
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router"
 import { SITE_DATA } from "@/data"
-import HomeView from "../views/HomeView.vue"
+/* Prepend import - DO NOT REMOVE */
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        {
-            path: "/",
-            name: "Homepage",
-            component: HomeView,
-            meta: { title: SITE_DATA.PAGE_TITLE("Homepage") },
-        },
-        {
-            path: "/about",
-            name: "About",
-            component: () => import("../views/AboutView.vue"),
-            meta: { title: SITE_DATA.PAGE_TITLE("Prout") },
-        },
-        /* Prepend here - DO NOT REMOVE */
+        /* Prepend route - DO NOT REMOVE */
     ],
 })
 
